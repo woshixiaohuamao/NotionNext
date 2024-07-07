c2bbcf435543c4dbaede56c2080f01a2584bad57
woshixiaohuamao <148666395+woshixiaohuamao@users.noreply.github.com>
2024-07-07 20:18:39 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -27,7 +27,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '牛鼻子的个人博客', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一位努力的IT码农', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://hexo.niu888.mom', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '🐱', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '1067865516@qq.com', // 邮箱地址 例如mail@tangly1024.com
